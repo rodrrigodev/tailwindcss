db8143af05a6b02868017e8e9e12b410748f6437
Rodrigo <rodrigosanyy@gmail.com>
2023-09-03 21:06:35 -0300

    navigation menu

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,15 +1,18 @@
 import { Search } from 'lucide-react'
 import { Logo } from './Logo'
+import { MainNavigation } from '../MainNavigation'
 
 export default function Sidebar() {
   return (
     <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
       <Logo />
       
-      <div className="flex  w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm mx-1">
         <Search className="h-5 w-5 text-zinc-500"/>
         <input className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Search"/>
       </div>
+
+      <MainNavigation/>
     </aside>
   )
 }
