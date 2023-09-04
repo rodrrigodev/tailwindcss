7e458267472e8293c3e6851840bd6fca465cbd2d
Rodrigo <rodrigosanyy@gmail.com>
2023-09-03 23:28:53 -0300

    widget of space

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,18 +1,48 @@
-import { Search } from 'lucide-react'
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  User,
+} from 'lucide-react'
 import { Logo } from './Logo'
-import { MainNavigation } from '../MainNavigation'
+import { NavItem } from './NavItem'
+import Home from '@/app/page'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export default function Sidebar() {
   return (
-    <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-      
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm mx-1">
-        <Search className="h-5 w-5 text-zinc-500"/>
-        <input className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Search"/>
+
+      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        <Search className="h-5 w-5 text-zinc-500" />
+        <input
+          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+          placeholder="Search"
+        />
       </div>
 
-      <MainNavigation/>
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={User} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+      </div>
     </aside>
   )
 }
