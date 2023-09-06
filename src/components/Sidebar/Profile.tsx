23721a37d499f393be7880af7b1c847735a27947
Rodrigo <rodrigosanyy@gmail.com>
2023-09-06 11:22:56 -0300

    sidebar dark

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,29 +1,23 @@
+import { Button } from '@/components/Button'
 import { LogOut } from 'lucide-react'
-import { Button } from '../Button'
 
 export function Profile() {
   return (
     <div className="grid grid-cols-profile items-center gap-3">
       <img
         src="https://github.com/rodrrigodev.png"
-        alt=""
         className="h-10 w-10 rounded-full"
+        alt=""
       />
-
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Rodrigo Santos
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           rodrigo@email.com
         </span>
       </div>
-
-      <Button
-        variant="ghost"
-        type="button"
-        className="ml-auto rounded-md p-2 hover:bg-zinc-50"
-      >
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
       </Button>
     </div>
